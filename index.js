1469474328c8ae860c02f06f95b73a049b8aa3e9
Muakhir <almuakhirjones@gmail.com>
2024-02-29 15:25:42 +0200

    Users/Update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) =>{
     res.header("Access-Control-Allow-Origin","*")
     res.header("Access-Control-Allow-Credentials","true")
     res.header("Access-Control-Allow-Methods","*")
-    res.header("Allow-Control-Request-Methods","*")
+    // res.header("Allow-Control-Request-Methods","*")
     res.header("Access-Control-Allow-Headers","*")
     res.header("Access-Control-Expose-Headers","Authorization")
     next();
