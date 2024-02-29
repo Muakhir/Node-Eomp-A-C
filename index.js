feae43833a7532d8d8f60ee44005a0b253f73954
Muakhir <almuakhirjones@gmail.com>
2024-02-29 15:30:49 +0200

    Users/Update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 const port = +process.env.PORT
 
 app.use((req, res, next) =>{
-    res.header("Access-Control-Allow-Origin","*")
+    res.header("Access-Control-Allow-Origin",'http://localhost:8080')
     res.header("Access-Control-Allow-Credentials","true")
     res.header("Access-Control-Allow-Methods","*")
     // res.header("Allow-Control-Request-Methods","*")
